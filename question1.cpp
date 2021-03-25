50288ea18af10f4ba5ee33be80b51fbdd9428bab
AdiBoye Philip <padiboye@gmail.com>
2021-03-25 23:52:47 +0000

    Fixed issues

--- question1.cpp
+++ question1.cpp
@@ -10,7 +10,7 @@ cin>> interest_rate;
 cout<<"Enter the duration in months:";
 cin>> duration;
 interest_rate=interest_rate + 0.2;
-face_value=amount_customer_needs/((1 - (interest_rate * duration_in_years));
+face_value=amount_customer_needs/(1 - (interest_rate * duration_in_years));
 duration_in_years=duration/12;
 cout<<"The face value is GHC"<<face_value;
 monthly_payment=face_value/duration;
@@ -22,7 +22,7 @@ goto repeat;}
 else{
 cout<<"That is all with the repetition.";
 repeat:
-face_value=amount_customer_needs/((1 – (interest_rate * duration_in_years));
+face_value=amount_customer_needs/(1 - (interest_rate * duration_in_years));
 cout<<"The face value is GHC"<<face_value;
 monthly_payment=face_value/duration;
 cout<<"The monthly payment is GHC"<<monthly_payment;
