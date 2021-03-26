22d593056afd83c7f7cc43bb4f8a08be37ff5ebc
AdiBoye Philip <padiboye@gmail.com>
2021-03-26 00:30:36 +0000

    fixed Logic

--- question1.cpp
+++ question1.cpp
@@ -9,23 +9,26 @@ cout<<"Enter the interest rate:";
 cin>> interest_rate;
 cout<<"Enter the duration in months:";
 cin>> duration;
-interest_rate=interest_rate + 0.2;
-face_value=amount_customer_needs/(1 - (interest_rate * duration_in_years));
 duration_in_years=duration/12;
-cout<<"The face value is GHC"<<face_value;
+face_value=amount_customer_needs/(1 - 0.12 * duration_in_years);
+cout<<"The face value is GHC"<<face_value<<endl;
 monthly_payment=face_value/duration;
-cout<<"The monthly payment is GHC"<<monthly_payment;
-cout<<"Enter the number for repetition:";
+cout<<"The monthly payment is GHC"<<monthly_payment<<endl;
+cout<<"Enter the number for repetition:"<<endl;
 cin>> i;
-if(i>0){
-goto repeat;}
-else{
-cout<<"That is all with the repetition.";
-repeat:
+for (int j = 0; j < i; j++ ){
+cout<<"Enter the amount customer needs:";
+cin>> amount_customer_needs;
+cout<<"Enter the interest rate:";
+cin>> interest_rate;
+cout<<"Enter the duration in months:";
+cin>> duration;
+duration_in_years=duration/12;
 face_value=amount_customer_needs/(1 - (interest_rate * duration_in_years));
 cout<<"The face value is GHC"<<face_value;
 monthly_payment=face_value/duration;
-cout<<"The monthly payment is GHC"<<monthly_payment;
+cout<<"\nThe monthly payment is GHC"<<monthly_payment << endl;
 }
+cout << "\nExecution Completed with " << i << " repitition" <<endl;
 return 0;
 }
